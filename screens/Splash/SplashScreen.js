d927dd959e503a7358d70815e6527e090a9000b2
Bhavana Patibandla <101495505+bavana112@users.noreply.github.com>
2022-04-24 06:28:38 -0600

    Update SplashScreen.js
    Added comments

--- screens/Splash/SplashScreen.js
+++ screens/Splash/SplashScreen.js
@@ -1,3 +1,4 @@
+//This page is used to show the user before initialization with an icon
 import { View, Text } from 'react-native'
 import React from 'react'
 import SplashScreen from 'react-native-splash-screen'
@@ -6,11 +7,13 @@ import { useNavigation } from '@react-navigation/native';
 const Splash = () => { 
   const navigation = useNavigation()
     useEffect(() => {
+     //splash screen will stay for 1 second
         setTimeout(() => {
           SplashScreen.hide();
           navigation.navigate('login')
         }, 1000);
       }, []);
+  //splash screen will hide after 1 second and navigates to login page automatically
   return (
     <View>
      
